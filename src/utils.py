3bec768b36b0c2def99d948e94133ab8e2a6c46d
shreyasaxena1 <shreyasaxena117@gmail.com>
2025-08-25 00:56:46 +0530

    Hyper Parameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
+from sklearn.model_selection  import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -21,7 +22,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(X_train, X_test, y_train, y_test, models):
+def evaluate_model(X_train, X_test, y_train, y_test, models, params):
     """
     Train and evaluate multiple models on given training and test data.
 
@@ -31,6 +32,7 @@ def evaluate_model(X_train, X_test, y_train, y_test, models):
         y_train: Training labels
         y_test: Test labels
         models: Dictionary of models to evaluate
+        params: Dict of paramter for model
 
     Returns:
         dict: Mapping of model name -> test r2_score
@@ -38,18 +40,30 @@ def evaluate_model(X_train, X_test, y_train, y_test, models):
     try:
         report = {}
 
-        for model_name, model in models.items():
-            model.fit(X_train, y_train)
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[model_name] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
         return report
+
 
     except Exception as e:
         raise CustomException(e, sys)
