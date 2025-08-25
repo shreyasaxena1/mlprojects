6125542b09d798c9cc11a110ac85768dff29cd25
shreyasaxena1 <shreyasaxena117@gmail.com>
2025-08-25 15:05:52 +0530

    UI

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pickle
 import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score
+import dill
 
 from src.exception import CustomException
 from sklearn.model_selection  import GridSearchCV
@@ -67,3 +68,12 @@ def evaluate_model(X_train, X_test, y_train, y_test, models, params):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def load_object(file_path):
+    #pickle.load(open('models/ridge.pkl','rb'))
+    
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
